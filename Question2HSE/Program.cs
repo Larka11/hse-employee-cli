6e1b5d63066e4c14ff4486ec757848421238e881
larka <larkaawolf@gmail.com>
2019-10-15 22:05:45 +0100

    Add all CS files so far

---
+++ Question2HSE/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question2HSE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program hseProgram = new Program();
+
+            hseProgram.PrintDetails();
+        }
+
+        private void PrintDetails()
+        {
+            hseEmployee employee1 = new hseEmployee();
+
+            Console.OutputEncoding = Encoding.GetEncoding(1252);
+
+            Console.WriteLine("employee name: " + employee1.empName);
+            Console.WriteLine("employee Number: " + employee1.empNumber);
+            Console.WriteLine("employee Type: " + employee1.empType);
+            Console.WriteLine("employee Yrs Service: " + employee1.empYrsService);
+            Console.WriteLine("employee Salary:" + "€" + employee1.empSalary);
+
+        }
+    }
+}
---
+++ Question2HSE/hseEmployee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question2HSE
+{
+    class hseEmployee
+    {
+        public String empName { get; set; }
+        public int empNumber {get; set;}
+        public string empType { get; set; }
+        public int empYrsService;
+        public double empSalary;
+
+        public hseEmployee( )
+        {
+            empName = "Becca";
+            empNumber = 100;
+            empType = "Doctor";
+            empYrsService = 20009328;
+            empSalary = 20000;
+        }
+
+    }
+}
